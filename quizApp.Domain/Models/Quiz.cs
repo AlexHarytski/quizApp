68f0f12be063b136e667063f74500d0b84d05676
AlexHarytski <a.harytski@andersenlab.com>
2019-12-11 21:04:38 +0300

    f

--- quizApp.Domain/Models/Quiz.cs
+++ quizApp.Domain/Models/Quiz.cs
@@ -3,6 +3,7 @@ namespace quizApp.Domain.Models
 {
     class Quiz
     {
+        //
         public string Id { get; set; }
         public string QuizTypeId { get; set; }
         public string Description { get; set; }
