14566942b24edfb1ccdeb59064658603330d96e0
AlexHarytski <a.harytski@andersenlab.com>
2019-12-11 20:49:24 +0300

    add class library; create domain model.

---
+++ quizApp.Domain/Models/QuizVariant.cs
@@ -0,0 +1,10 @@
+﻿namespace quizApp.Domain.Models
+{
+    public class QuizVariant
+    {
+        public string Id { get; set; }
+        public string VariantText { get; set; }
+        public bool IsCorrect { get; set; }
+        public string QuizQuestionId { get; set; }
+    }
+}
---
+++ quizApp.Domain/Models/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace quizApp.Domain.Models
+{
+    public class User
+    {
+        //
+        public string Id { get; set; }
+        public string Email { get; set; }
+        public string PasswordHash { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateRegistration { get; set; }
+    }
+}
