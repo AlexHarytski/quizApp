e956982cf6e4d758045bdfe07a494cd73f707e26
AlexHarytski <a.harytski@andersenlab.com>
2019-12-11 21:07:13 +0300

    F

--- quizApp.Domain/Models/QuizType.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace quizApp.Domain.Models
-{
-    public class QuizType
-    {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-    }
-}
