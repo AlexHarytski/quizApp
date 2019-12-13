bb733f85bbe7794138b640a77f5e35ce6cebfeb1
AlexHarytski <a.harytski@andersenlab.com>
2019-12-13 12:19:11 +0300

    Add unit of work

---
+++ quizApp.Persistence/QuizUOW.cs
@@ -0,0 +1,16 @@
+﻿namespace quizApp.Persistence
+{
+    public class QuizUow
+    {
+        public QuizRepository QuizRepository { get; set; }
+        public UserRepository UserRepository { get; set; }
+        public QuizResultRepository QuizResultRepository { get; set; }
+
+        public QuizUow(IQuizDatabaseSettings dbSettings)
+        {
+            QuizRepository = new QuizRepository(dbSettings);
+            UserRepository = new UserRepository(dbSettings);
+            QuizResultRepository = new QuizResultRepository(dbSettings);
+        }
+    }
+}
